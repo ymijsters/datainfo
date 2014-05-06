90df9a0ce0c42b03287d4d1b0fc1a3637f29a965
ymijsters <s1499459@utwente.nl>
2014-05-06 15:23:40 +0200

    excersice2.AB

--- session2/exercise2.sql
+++ session2/exercise2.sql
@@ -1,3 +1,14 @@
 CREATE TABLE R (A INT, B INT, C, INT, D INT, E INT, F INT);
 CREATE TABLE F (x VARCHAR(10), y VARCHAR(10),
 violation BOOLEAN, motivation VARCHAR(200));
+INSERT INTO F("abc", "ef",True,"ABC->EF en E->D dus ABC is een superkey");
+INSERT INTO F("e", "d",False,"E->D en D->A, maar B,C en F niet dus E is geen superkey");
+INSERT INTO F("d", "a",False,"D->A en voor de rest niet dus D is geen superkey");
+CREATE TABLE R1 (E INT, D INT);
+CREATE TABLE R2 (A INT, B INT, C INT, E INT, F INT);
+CREATE TABLE F1 (x VARCHAR(10), y VARCHAR(10),
+violation BOOLEAN, motivation VARCHAR(200));
+INSERT INTO F1 VALUES("E","D",TRUE, "e+=ed");
+CREATE TABLE F2 (x VARCHAR(10), y VARCHAR(10),
+violation BOOLEAN, motivation VARCHAR(200));
+INSERT INTO F2 VALUES("ABC","EF",TRUE, "ABC+=ABCEF");
