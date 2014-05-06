f15acb6e6c7593d0c5ac3091a19d19dbab1a5ac7
ymijsters <s1499459@utwente.nl>
2014-05-06 14:53:44 +0200

    excersice1

---
+++ session2/result1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Problem1 (
+task CHAR(1) PRIMARY KEY, result BOOLEAN, motivation VARCHAR(200));
+INSERT INTO Problem1 (a, False, "er kunnen verschillende auto’s zijn met dezelfde capaciteit");
+INSERT INTO Problem1 (b, True, "een auto kan maar een capaciteit hebben.");
+INSERT INTO Problem1 (c, False, "er kunnen verschillende auto’s zijn met dezelfde capaciteit");
+INSERT INTO Problem1 (d, True, , "(a) een pakket kan maar op een factuur staan en kan maar op een adres worden afgeleverd");
+INSERT INTO Problem1 (e, True, "een pakket heeft maar 1 geldbedrag en maar 1 ontvanger");
+INSERT INTO Problem1 (f, False, "uit 1 en 2 blijkt niet dat een bezorger maar 1 auto heeft");
+INSERT INTO Problem1 (g, False, "een auto met een bepaalde capaciteit kan door meerdere bezorgers worden gebruikt");
+INSERT INTO Problem1 (h, True, "B is alleen afhankelijk voor V");
+INSERT INTO Problem1 (i, True, "C is alleen afhankelijk van V");
+INSERT INTO Problem1 (j, False, "als h en i waar zijn dan deze ook");
+
